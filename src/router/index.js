77a1a39100028b2832cfb3a8a4f0569a968f90c7
krishna <krishna.nature91@gmail.com>
2020-12-05 22:16:31 +0000

    Added necessary components for album pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUpPage from '../views/SignUpPage.vue'
+import AlbumDetailPage from '../views/AlbumDetailPage.vue'
+import AlbumsPage from '../views/AlbumsPage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +12,23 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: "/signup",
+    name: "SignUpPage",
+    component: SignUpPage
+  },
+  {
+    path: "/album/:id",
+    name: "AlbumDetailPage",
+    component: AlbumDetailPage,
+    meta: {requiresAuth: true}
+  },
+  {
+    path: "/albums",
+    name: "AlbumsPage",
+    component: AlbumsPage,
+    meta: {requiresAuth: true}
   },
   {
     path: '/about',
---
+++ src/views/AlbumDetailPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Album Detail Page</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
