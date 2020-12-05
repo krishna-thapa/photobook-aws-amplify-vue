901ba467b3db7590ba30e99245ce9188e9cfc591
krishna <krishna.nature91@gmail.com>
2020-12-05 22:07:35 +0000

    Added login form with tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home"> 
-    Hello World
+    <Login />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Login from '../components/Login.vue'
 
 export default {
   name: 'Home',
-  components: {}
-}
+  components: {
+    Login,
+  },
+};
 </script>
