0af914df715ede2f9bfcda738c731cb58c155b7c
krishna <krishna.nature91@gmail.com>
2020-12-05 23:00:44 +0000

    Nav bar is added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
+      <Nav />
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Nav from "@/components/Nav.vue"
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+
 
 <style lang="scss">
 #app {
