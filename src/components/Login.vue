c8214f41b5049b3d75e95f8e9f8485dc6b0b8a87
krishna <krishna.nature91@gmail.com>
2020-12-05 20:58:38 +0000

    Create a new login componenet

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    date: () => ({
+      username: "",
+      password: "",
+      email: "",
+      error: ""
+    })
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
